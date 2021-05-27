c2fc6bcbb291a21675237e255f2ab02f6df7b79b
Pingjing <fish1063345@gmail.com>
2021-05-27 20:22:34 +0800

    [New][1365] Add 1365

---
+++ 1365.How-many-numbers-are-smaller-than-the-current-number/How-many-numbers-are-smaller-than-the-current-number.go
@@ -0,0 +1,21 @@
+package problem1365
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	output := make([]int, len(nums))
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		count = compare(nums, i)
+		output[i] = count
+	}
+	return output
+}
+
+func compare(nums []int, index int) int {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < nums[index] {
+			count += 1
+		}
+	}
+	return count
+}
