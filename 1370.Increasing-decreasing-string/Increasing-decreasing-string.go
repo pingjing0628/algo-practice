fd564192720d957a8ff3e2ff935e17bd86062b42
Pingjing <fish1063345@gmail.com>
2021-06-03 16:40:45 +0800

    [Update][1370]add comment

--- 1370.Increasing-decreasing-string/Increasing-decreasing-string.go
+++ 1370.Increasing-decreasing-string/Increasing-decreasing-string.go
@@ -4,11 +4,15 @@ func sortString(s string) string {
 	counts := make([]int, 26)
 	stringLength := len(s)
 	result := ""
+
+	// make count in counts
+	// [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	for _, value := range s {
 		counts[value - 'a']++
 	}
 
 	for stringLength > 0 {
+		// smallest
 		for i := 0; i < 26; i++{
 			if counts[i] > 0{
 				result += string(i + 'a')
@@ -17,6 +21,7 @@ func sortString(s string) string {
 			}
 		}
 
+		// largest
 		for i := 25; i >= 0; i-- {
 			if counts[i] > 0{
 				result += string(i + 'a')
---
+++ 1436.Destination-city/Destination-city.go
@@ -0,0 +1,1 @@
+package _436_Destination_city
