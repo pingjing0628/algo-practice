80b94c333b6ded9336c50e387583470f39c1f293
Pingjing <fish1063345@gmail.com>
2021-06-29 17:35:48 +0800

    [New][1051] sort

---
+++ 1051.Height-checker/Height-checker.go
@@ -0,0 +1,18 @@
+package problem1051
+
+import "sort"
+
+func heightChecker(heights []int) int {
+	count := 0
+	expected := make([]int, len(heights))
+	copy(expected, heights)
+
+	sort.Ints(expected)
+
+	for i := 0; i < len(heights);i++{
+		if heights[i] != expected[i] {
+			count++
+		}
+	}
+	return count
+}
